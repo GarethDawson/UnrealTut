39595bbdac18a9cbb0a1b853d713a2de28f72c38
GarethDawson <garethpdawson@gmail.com>
2014-06-30 15:43:42 +0100

    Setting up new project
    New unreal project

---
+++ Source/UnrealTut/UnrealTut.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTut.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTut, "UnrealTut" );
+ 
---
+++ Source/UnrealTut/UnrealTut.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __UNREALTUT_H__
+#define __UNREALTUT_H__
+
+#include "Engine.h"
+
+#endif
